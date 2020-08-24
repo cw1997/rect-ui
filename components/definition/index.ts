b4264edf92a0aaacb6cd8099377d44d67894d020
昌维 <867597730@qq.com>
2020-08-24 17:12:29 +0800

    add size and color enum definition, and class name prefix constant to typescript

---
+++ components/definition/index.ts
@@ -0,0 +1,20 @@
+export const prefixCls = 'rect';
+
+// const prefixClsSize = 'size';
+export enum SizeEnum {
+    xl   =   'size-xl',
+    lg   =   'size-lg',
+    md   =   'size-md',
+    sm   =   'size-sm',
+    xs   =   'size-xs',
+}
+
+// const prefixClsColor = 'color';
+export enum ColorEnum {
+    default =   'color-default',
+    primary =   'color-primary',
+    line    =   'color-line',
+    success =   'color-success',
+    warning =   'color-warning',
+    danger  =   'color-danger',
+}
